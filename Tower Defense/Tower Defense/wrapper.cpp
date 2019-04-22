b6e637b2ddf7718e8d8fb6d81aaf0ae9f61f21e3
Michael <mjhfraud@gmail.com>
2019-04-22 15:00:15 -0700

    map creation in wrapper

--- Tower Defense/Tower Defense/wrapper.cpp
+++ Tower Defense/Tower Defense/wrapper.cpp
@@ -19,12 +19,65 @@ wrapper::~wrapper() {
 void wrapper::run_app() {
 
 
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Tower Defense");
+	sf::RenderWindow window(sf::VideoMode(1400, 950), "Tower Defense");
 
 
-	sf::CircleShape shape(200.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(250, 50));
+	rectangle.setOutlineColor(sf::Color::Black);
+	rectangle.setOutlineThickness(0);
+	rectangle.setPosition(0, 0);
+	rectangle.setFillColor(sf::Color::Black);
 
+	sf::RectangleShape rectangle2;
+	rectangle2.setSize(sf::Vector2f(50, 500));
+	rectangle2.setOutlineColor(sf::Color::Black);
+	rectangle2.setOutlineThickness(0);
+	rectangle2.setPosition(250, 0);
+	rectangle2.setFillColor(sf::Color::Black);
+
+
+	sf::RectangleShape rectangl3;
+	rectangl3.setSize(sf::Vector2f(500, 50));
+	rectangl3.setOutlineColor(sf::Color::Black);
+	rectangl3.setOutlineThickness(0);
+	rectangl3.setPosition(250, 500);
+	rectangl3.setFillColor(sf::Color::Black);
+
+
+
+	sf::RectangleShape rectangl4;
+	rectangl4.setSize(sf::Vector2f(50, 250));
+	rectangl4.setOutlineColor(sf::Color::Black);
+	rectangl4.setOutlineThickness(0);
+	rectangl4.setPosition(700, 500);
+	rectangl4.setFillColor(sf::Color::Black);
+
+
+	sf::RectangleShape rectangl5;
+	rectangl5.setSize(sf::Vector2f(1000, 50));
+	rectangl5.setOutlineColor(sf::Color::Black);
+	rectangl5.setOutlineThickness(0);
+	rectangl5.setPosition(750, 700);
+	rectangl5.setFillColor(sf::Color::Black);
+
+
+
+	sf::Texture BackgroundTexture;
+	sf::Sprite background;
+	sf::Vector2u TextureSize;  //Added to store texture size.
+	sf::Vector2u WindowSize;   //Added to store window size.
+
+	BackgroundTexture.loadFromFile("grass.png");
+
+	TextureSize = BackgroundTexture.getSize(); //Get size of texture.
+	WindowSize = window.getSize();             //Get size of window.
+
+	float ScaleX = (float)WindowSize.x / TextureSize.x;
+	float ScaleY = (float)WindowSize.y / TextureSize.y;     //Calculate scale.
+
+	background.setTexture(BackgroundTexture);
+	background.setScale(ScaleX, ScaleY);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -35,7 +88,13 @@ void wrapper::run_app() {
 		}
 
 		window.clear();
-		window.draw(shape);
+		
+		window.draw(background);
+		window.draw(rectangle);
+		window.draw(rectangle2);
+		window.draw(rectangl3);
+		window.draw(rectangl4);
+		window.draw(rectangl5);
 		window.display();
 	}
 }
