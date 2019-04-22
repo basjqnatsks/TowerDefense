f63849758e6960930eae4e46359672ec28ac8a24
bandyalva <alva.d.bandy.jr@gmail.com>
2019-04-22 16:53:05 -0700

    enemy.h

--- Tower Defense/Tower Defense/Enemy.h
+++ Tower Defense/Tower Defense/Enemy.h
@@ -1,12 +1,14 @@
 #pragma once
 #include"Entity.h"
-class Enemy{
+class Enemy : public Entity {
 public:
 	Enemy();
 	~Enemy();
-
+	Enemy(float killValue, float hp);
+	void pathing(float stopCoords[][50]);
+	void onHit();
 
 private:
-
-
+	float killValue;
+	float hp;
 };
