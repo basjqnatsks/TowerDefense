d7a101509ea128ebd23f49480f89eebb950ac0e0
basjqnatsks <mjhfraud@gmail.com>
2019-04-23 00:31:14 -0700

    Network

--- Tower Defense/Tower Defense/main.cpp
+++ Tower Defense/Tower Defense/main.cpp
@@ -3,10 +3,11 @@
 #include "UI.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <Windows.h>
+#include <iostream>
 int main() {
-	wrapper app;
-	app.run_app();
 
+	std::cout << system("curl -i -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X GET https://raw.githubusercontent.com/basjqnatsks/TowerDefense/master/Tower%20Defense/Menu.txt");
 
 	return 0;
 
